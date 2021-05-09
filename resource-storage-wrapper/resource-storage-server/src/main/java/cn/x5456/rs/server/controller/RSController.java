f9037386e96c1ad512fa2bff62f7705286d3921a
yujx <yujx@dist.com.cn>
2021-05-09 09:34:09 +0800

    删除 v1 版本的文件下载

--- resource-storage-wrapper/resource-storage-server/src/main/java/cn/x5456/rs/server/controller/RSController.java
+++ resource-storage-wrapper/resource-storage-server/src/main/java/cn/x5456/rs/server/controller/RSController.java
@@ -1,8 +1,6 @@
 package cn.x5456.rs.server.controller;
 
 import cn.hutool.core.util.IdUtil;
-import cn.x5456.infrastructure.util.DataBufferUtilsExt;
-import cn.x5456.infrastructure.util.FileDownloadUtil;
 import cn.x5456.rs.def.IResourceStorage;
 import cn.x5456.rs.entity.ResourceInfo;
 import io.swagger.annotations.Api;
@@ -12,20 +10,16 @@ import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.buffer.DataBuffer;
 import org.springframework.core.io.buffer.DataBufferFactory;
-import org.springframework.core.io.buffer.DataBufferUtils;
-import org.springframework.http.*;
+import org.springframework.http.ContentDisposition;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.codec.multipart.FilePart;
-import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-import java.io.File;
 import java.nio.charset.StandardCharsets;
-import java.util.List;
-
-import static cn.x5456.rs.constant.DataBufferConstant.DEFAULT_CHUNK_SIZE;
 
 /**
  * @author yujx
@@ -52,77 +46,6 @@ public class RSController {
     }
 
     // TODO: 2021/5/8 好像是不写 content-type 就直接下载，写了才会预览 -> 当我没说，不知道，到时候学习下
-    /*
-    curl -i -H 'Range: bytes=1-7' 'http://127.0.0.1:8080/v1/download/6096804ecdb2eff210599827'
-    curl -i -H 'Range: bytes=7-' 'http://127.0.0.1:8080/v1/download/6096804ecdb2eff210599827'
-    curl -i -H 'Range: bytes=0-2, 2-5' 'http://127.0.0.1:8080/v1/download/6096804ecdb2eff210599827'
-
-    https://developer.mozilla.org/zh-CN/docs/Web/HTTP/Range_requests
-
-    GetMapping 也可以接收 HEAD 请求的，所以需要在响应头中加上 Accept-Ranges: bytes 和 Content-Length: 146515
-     */
-    @Deprecated
-    @ApiOperation("下载/预览文件")
-    @GetMapping("/v1/download/{path}")
-    public Mono<Void> download(@PathVariable String path,
-                               ServerHttpRequest request, ServerHttpResponse response) {
-        return resourceStorage.downloadFile(path)
-                .flatMap(pair -> {
-                    String fileName = pair.getKey();
-                    String localFilePath = pair.getValue();
-
-                    // 获取 range
-                    HttpHeaders requestHeaders = request.getHeaders();
-                    List<HttpRange> rangeList = requestHeaders.getRange();
-                    if (rangeList.size() > 1) {
-                        response.setStatusCode(HttpStatus.REQUESTED_RANGE_NOT_SATISFIABLE); // 这个未生效
-                        return Mono.error(new RuntimeException("请求头中 range 的数量不合法！"));
-                    }
-
-                    // 设置文件名的响应头
-                    HttpHeaders responseHeaders = response.getHeaders();
-                    ContentDisposition disposition = ContentDisposition.builder("attachment")
-                            .filename(fileName, StandardCharsets.UTF_8)
-                            .build();
-                    responseHeaders.setContentDisposition(disposition);
-                    responseHeaders.add(HttpHeaders.ACCEPT_RANGES, "bytes");
-
-                    // 如果没有设置 range 则全部返回
-                    File file = new File(localFilePath);
-                    long length = file.length();
-                    if (rangeList.size() == 0) {
-                        responseHeaders.setContentLength(length);
-                        if (response instanceof ZeroCopyHttpOutputMessage) {
-                            return ((ZeroCopyHttpOutputMessage) response).writeWith(file, 0, length);
-                        }
-                        Flux<DataBuffer> bufferFlux = DataBufferUtils.read(
-                                new FileSystemResource(localFilePath), dataBufferFactory, DEFAULT_CHUNK_SIZE);
-                        return response.writeWith(bufferFlux);
-                    }
-
-                    // 获取请求头中的 range
-                    HttpRange range = rangeList.get(0);
-
-                    // 获取 range 的范围
-                    long rangeStart = range.getRangeStart(length);
-                    long rangeEnd = range.getRangeEnd(length);
-
-                    // 写响应头
-                    responseHeaders.add(HttpHeaders.CONTENT_RANGE, FileDownloadUtil.createContentRange(range, length));
-                    responseHeaders.setContentLength(rangeEnd - rangeStart);
-                    responseHeaders.setContentType(MediaType.APPLICATION_OCTET_STREAM);
-
-                    // 设置响应状态码
-                    response.setStatusCode(HttpStatus.PARTIAL_CONTENT);
-
-                    if (response instanceof ZeroCopyHttpOutputMessage) {
-                        return ((ZeroCopyHttpOutputMessage) response).writeWith(file, rangeStart, rangeEnd);
-                    }
-                    Flux<DataBuffer> bufferFlux = DataBufferUtilsExt.read(
-                            new FileSystemResource(localFilePath), rangeStart, rangeEnd, dataBufferFactory, DEFAULT_CHUNK_SIZE);
-                    return response.writeWith(bufferFlux);
-                });
-    }
 
     /*
     参考资料：
