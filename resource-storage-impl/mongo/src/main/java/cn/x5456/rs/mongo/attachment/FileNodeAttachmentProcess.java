6d6c74a76fe1b550f7efc1806a4a72fdfdca3d1e
yujx <yujx@dist.com.cn>
2021-05-13 17:09:41 +0800

    fix：优化 FileNodeAttachmentProcess

--- resource-storage-impl/mongo/src/main/java/cn/x5456/rs/mongo/attachment/FileNodeAttachmentProcess.java
+++ resource-storage-impl/mongo/src/main/java/cn/x5456/rs/mongo/attachment/FileNodeAttachmentProcess.java
@@ -12,6 +12,7 @@ import cn.x5456.rs.attachment.AttachmentProcessContainer;
 import cn.x5456.rs.constant.AttachmentConstant;
 import cn.x5456.rs.def.IResourceStorage;
 import cn.x5456.rs.entity.FileMetadata;
+import cn.x5456.rs.mongo.document.FsFileMetadata;
 import cn.x5456.rs.mongo.document.FsResourceInfo;
 import cn.x5456.rs.mongo.dto.ZipFileNode;
 import com.google.common.collect.ImmutableList;
@@ -55,42 +56,45 @@ public class FileNodeAttachmentProcess implements AttachmentProcess<ZipFileNode>
 
     @Override
     public ZipFileNode process(FileMetadata metadata) {
-        return mongoResourceStorage.downloadFileByFileHash(metadata.getFileHash())
-                .publishOn(scheduler)   // 切换到普通线程，不能阻塞 nio 线程（指的不是下面的 block()）
-                .flatMap(localFilePath -> {
-
+        Mono<ZipFileNode> zipFileNodeMono = Mono.create(sink -> mongoResourceStorage.downloadFileByFileHash(metadata.getFileHash())
+                .subscribe(localFilePath -> {
                     // 猜测文件类型
-                    Query query = new Query(Criteria.where("fileHash").is(metadata.getFileHash()));
-                    FsResourceInfo resourceInfo = mongoTemplate.findOne(query, FsResourceInfo.class).block();
+                    Query query = new Query(Criteria.where(FsFileMetadata.FILE_HASH).is(metadata.getFileHash()));
+                    mongoTemplate.findOne(query, FsResourceInfo.class)
+                            .publishOn(scheduler)   // 切换到普通线程，不能阻塞 nio 线程（指的不是下面的 block()）
+                            .subscribe(resourceInfo -> {
+                                // 推测文件的类型，并保存
+                                String fileType = FileTypeGuessUtil.getTypeByPath(localFilePath, resourceInfo.getFileName());
+                                metadata.getAttachments().put(AttachmentConstant.FILE_TYPE, fileType);
+                                mongoTemplate.save(metadata).subscribe();
 
-                    String fileType = FileTypeGuessUtil.getTypeByPath(localFilePath, resourceInfo.getFileName());
-                    metadata.getAttachments().put(AttachmentConstant.FILE_TYPE, fileType);
-                    mongoTemplate.save(metadata).subscribe();
+                                // 如果文件是压缩包，则解析其中文件结构
+                                if (!COMPRESS_PACKAGE_TYPE_LIST.contains(fileType)) {
+                                    sink.error(new RuntimeException("该文件不是压缩包，无法解析！"));
+                                    return;
+                                }
 
-                    // 如果文件是压缩包，则解析其中文件结构
-                    if (!COMPRESS_PACKAGE_TYPE_LIST.contains(fileType)) {
-                        return Mono.error(new RuntimeException("该文件不是压缩包，无法解析！"));
-                    }
+                                // 解压并解析
+                                String extractPath = CompressUtils.extract(localFilePath);
+                                FileNodeDTO fileNode = FileNodeUtil.getFileNode(extractPath, node -> {
+                                    String path = IdUtil.objectId();
+                                    mongoResourceStorage.uploadFile(localFilePath, path).subscribe();
+                                    node.addAttachment(ZipFileNode.PATH, path);
+                                });
 
-                    String extractPath = CompressUtils.extract(localFilePath);
-                    FileNodeDTO fileNode = FileNodeUtil.getFileNode(extractPath, node -> {
-                        String path = IdUtil.objectId();
-                        mongoResourceStorage.uploadFile(localFilePath, path).subscribe();
-                        node.addAttachment(ZipFileNode.PATH, path);
-                    });
+                                // 将 fileNode 映射为 zipFileNode
+                                ZipFileNode zipFileNode = BeanUtil.toBean(fileNode.getAttachments(), ZipFileNode.class);
+                                BeanUtil.copyProperties(fileNode, zipFileNode);
+                                metadata.getAttachments().put(AttachmentConstant.FILE_NODE, zipFileNode);
+                                mongoTemplate.save(metadata).subscribe(x -> log.info("压缩包文件解析成功！"));
 
-                    // 将 fileNode 映射为 zipFileNode
-                    ZipFileNode zipFileNode = BeanUtil.toBean(fileNode.getAttachments(), ZipFileNode.class);
-                    BeanUtil.copyProperties(fileNode, zipFileNode);
+                                // 删除解压出来的压缩包
+                                FileUtil.del(extractPath);
 
-                    metadata.getAttachments().put(AttachmentConstant.FILE_NODE, zipFileNode);
-                    mongoTemplate.save(metadata).subscribe();
-                    log.info("压缩包文件解析成功！");
+                                sink.success(zipFileNode);
+                            });
+                }));
 
-                    // 删除解压出来的压缩包
-                    FileUtil.del(extractPath);
-
-                    return Mono.just(zipFileNode);
-                }).block();
+        return zipFileNodeMono.block();
     }
 }
