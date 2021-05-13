cc29cd40ab8cd6321ddf776bddffc8ca326848af
dengdh <dengdh167@163.com>
2021-05-13 17:19:25 +0800

    fix: 改用hutool工具类实现一致性hash

--- resource-storage-wrapper/resource-storage-sdk/resource-storage-gateway/src/main/java/cn/x5456/rs/gateway/route/MyRoundRobinLoadBalancer.java
+++ resource-storage-wrapper/resource-storage-sdk/resource-storage-gateway/src/main/java/cn/x5456/rs/gateway/route/MyRoundRobinLoadBalancer.java
@@ -1,12 +1,14 @@
 package cn.x5456.rs.gateway.route;
 
-import cn.x5456.infrastructure.util.HashUtil;
+import cn.hutool.core.lang.ConsistentHash;
+import cn.hutool.core.util.ObjectUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.loadbalancer.reactive.DefaultResponse;
-import org.springframework.cloud.client.loadbalancer.reactive.EmptyResponse;
-import org.springframework.cloud.client.loadbalancer.reactive.Request;
-import org.springframework.cloud.client.loadbalancer.reactive.Response;
+import org.springframework.cloud.client.loadbalancer.DefaultResponse;
+import org.springframework.cloud.client.loadbalancer.EmptyResponse;
+import org.springframework.cloud.client.loadbalancer.Request;
+import org.springframework.cloud.client.loadbalancer.Response;
 import org.springframework.cloud.loadbalancer.core.NoopServiceInstanceListSupplier;
 import org.springframework.cloud.loadbalancer.core.RoundRobinLoadBalancer;
 import org.springframework.cloud.loadbalancer.core.ServiceInstanceListSupplier;
@@ -14,12 +16,6 @@ import org.springframework.util.CollectionUtils;
 import reactor.core.publisher.Mono;
 
 import java.util.List;
-import java.util.Set;
-import java.util.SortedMap;
-import java.util.TreeMap;
-import java.util.concurrent.ConcurrentMap;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 /**
  * @author: dengdh@dist.com.cn
@@ -28,26 +24,24 @@ import java.util.stream.IntStream;
  * 这里原本想参照 {@link RoundRobinLoadBalancer} 来重写choose和getInstanceResponse的，但是请看构造函数里的两个参数从 Environment 中获取失败，
  * 所以采用了另一种方式：MyReactiveLoadBalancerClientFilter + MyRoundRobinLoadBalancer
  */
-public class MyRoundRobinLoadBalancer extends RoundRobinLoadBalancer {
+@Slf4j
+public class MyRoundRobinLoadBalancer {
 
     private ObjectProvider<ServiceInstanceListSupplier> serviceInstanceListSupplierProvider;
 
     private List<ServiceInstance> instanceList;
 
-    private ConcurrentMap<String, List<ServiceInstance>> serviceMap;
+    private final static int VIRTUAL_NODE_COUNT = 5;
 
-    private final static int virtualNode = 5;
-    /**
-     * 各个虚拟节点集合
-     */
-    private static SortedMap<Integer, String> virtualNodeMap = new TreeMap<>();
+    private ConsistentHash consistentHash;
+
+    private String serviceId;
 
     public MyRoundRobinLoadBalancer(ObjectProvider<ServiceInstanceListSupplier> serviceInstanceListSupplierProvider, String serviceId) {
-        super(serviceInstanceListSupplierProvider, serviceId);
+        this.serviceId = serviceId;
         this.serviceInstanceListSupplierProvider = serviceInstanceListSupplierProvider;
     }
 
-    @Override
     public Mono<Response<ServiceInstance>> choose(Request request) {
         if (serviceInstanceListSupplierProvider != null) {
             ServiceInstanceListSupplier supplier = serviceInstanceListSupplierProvider
@@ -65,13 +59,18 @@ public class MyRoundRobinLoadBalancer extends RoundRobinLoadBalancer {
     private Response<ServiceInstance> getInstanceResponse(
             List<ServiceInstance> instances, Request request) {
         if (instances.isEmpty()) {
+            log.warn("{}没有可用的实例", serviceId);
             return new EmptyResponse();
         }
 
         // 判断服务是否为空
         if (CollectionUtils.isEmpty(instanceList)) {
             instanceList = instances;
-            buildVirtualNodeMap();
+        }
+
+        // 构造hash环
+        if (ObjectUtil.isNull(consistentHash)) {
+            consistentHash = new ConsistentHash<>(VIRTUAL_NODE_COUNT, instanceList);
         }
 
         // 判断服务是否有更新
@@ -79,58 +78,19 @@ public class MyRoundRobinLoadBalancer extends RoundRobinLoadBalancer {
         if (update) {
             instanceList.clear();
             instanceList = instances;
-            buildVirtualNodeMap();
+            // 直接重构hansh环
+            consistentHash = new ConsistentHash<>(VIRTUAL_NODE_COUNT, instanceList);
         }
 
+        // 根据key取第一个节点
         if (request instanceof MyRequest) {
             MyRequest myRequest = (MyRequest) request;
-            String server = getServer(myRequest.getHost());
-            ServiceInstance serviceInstance = serviceMap.get(server).get(0);
+            ServiceInstance serviceInstance = (ServiceInstance) consistentHash.get(myRequest.getHost());
             return new DefaultResponse(serviceInstance);
         }
 
         // 否则就默认拿第一个
         ServiceInstance instance = instanceList.get(0);
         return new DefaultResponse(instance);
-    }
-
-    /**
-     * 获取服务的 key
-     *
-     * @param node 客户端 ip
-     * @return 服务实例的 key
-     */
-    private String getServer(String node) {
-        Integer nodeCode = HashUtil.FNV1_32_HASH(node);
-        // 获取大于当前hash值的节点集合
-        SortedMap<Integer, String> tailMap = virtualNodeMap.tailMap(nodeCode);
-        Integer key;
-        if (tailMap.isEmpty()) {
-            key = virtualNodeMap.firstKey();
-        } else {
-            key = tailMap.firstKey();
-        }
-        // 拿到值
-        String virtualNodeValue = virtualNodeMap.get(key);
-        return virtualNodeValue.split("&&")[0];
-    }
-
-    /**
-     * 构建虚拟节点
-     */
-    private void buildVirtualNodeMap() {
-        virtualNodeMap.clear();
-        // 根据服务实例id分组   instanceId：192.168.1.99:8501
-        serviceMap = instanceList.stream().collect(Collectors.groupingByConcurrent(ServiceInstance::getInstanceId));
-        Set<String> keySet = serviceMap.keySet();
-
-        // 虚拟节点
-        keySet.forEach(k -> {
-            IntStream.range(0, 4).forEach(i -> {
-                String virtualNodeName = k + "&&Node" + i;
-                Integer hashCode = HashUtil.FNV1_32_HASH(virtualNodeName);
-                virtualNodeMap.put(hashCode, virtualNodeName);
-            });
-        });
     }
 }
