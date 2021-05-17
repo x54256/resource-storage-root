50d01b289c620c5510ea18f0b2be79a5913efdd1
yujx <yujx@dist.com.cn>
2021-05-17 14:07:05 +0800

    fix: 将 webclient 调整成正常的 jar 包的结构

---
+++ resource-storage-wrapper/resource-storage-sdk/resource-storage-webclient-sdk/src/main/java/cn/x5456/rs/sdk/webclient/BootstrapConfig.java
@@ -0,0 +1,11 @@
+package cn.x5456.rs.sdk.webclient;
+
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * @author yujx
+ * @date 2021/01/15 09:26
+ */
+@ComponentScan
+public class BootstrapConfig {
+}
---
+++ resource-storage-wrapper/resource-storage-sdk/resource-storage-webclient-sdk/src/test/java/cn/x5456/rs/sdk/webclient/client/Config.java
@@ -0,0 +1,13 @@
+package cn.x5456.rs.sdk.webclient.client;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author yujx
+ * @date 2021/05/17 14:05
+ */
+@Configuration
+@EnableAutoConfiguration
+public class Config {
+}
